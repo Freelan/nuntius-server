b98f50eb309761c840dba32a84fa74a36519d8c4
Freelan <lunghshtern@gmail.com>
2016-04-23 14:10:18 +0200

    Somehow it works.

--- main.cpp
+++ main.cpp
@@ -25,35 +25,37 @@ std::string receiveMessage( sf::TcpSocket& socket )
 int main()
 {
     // ----- The server -----
+    sf::TcpSocket socket;
     sf::TcpListener listener;
     listener.listen(55001);
     std::cout << "Listening..." << std::endl;
-    
-    sf::TcpSocket socket;
 
+    std::vector<sf::IpAddress>addresses;
     std::vector < sf::TcpSocket *> clients;
     sf::SocketSelector selector;
     selector.add( listener );
 
-    std::vector<sf::IpAddress>addresses;
-
     while( true )
     {
-        if( selector.wait( sf::seconds( 2 ) ) ) 
+        if( selector.wait( sf::seconds( 2 ) ) )
         {
             if( selector.isReady( listener ) )
             {
                 sf::TcpSocket * tmp = new sf::TcpSocket;
-                listener.accept( * tmp );
-                clients.push_back( tmp );
-                selector.add( * tmp );
+                if( listener.accept( * tmp )  == sf::Socket::Done )
+                {
+                    clients.push_back( tmp );
+                    selector.add( * tmp );
 
-                sf::IpAddress tmpIp = tmp->getRemoteAddress();
-                addresses.push_back( tmpIp );
+                    sf::IpAddress tmpIp = tmp->getRemoteAddress();
+                    addresses.push_back( tmpIp );
 
-                std::cout << "New client connected: " << tmp->getRemoteAddress() << std::endl;
-                //sendMessage( *tmp, tmpIp.toString() );
-                sendMessage( *tmp, "Connected!" );
+                    std::cout << "New client connected: " << tmp->getRemoteAddress() << std::endl;
+                    sendMessage( *tmp, "\r-- CONNECTED --" );
+                }else
+                {
+                    delete tmp;
+                }
             }
 
             for( int i = 0; i < clients.size(); i++ )
@@ -67,13 +69,20 @@ int main()
                     {
                         std::cout << clients[i]->getRemoteAddress() << " disconnected!" << std::endl;
                         selector.remove( *clients[i] );
+                        clients[i]->disconnect();
+                        clients.erase( clients.begin() + i );
+                        addresses.erase( addresses.begin() + i );
                     }else
                     {
                         std::cout << "[" << clients[i]->getRemoteAddress() << "] " << receivedMessage << std::endl;
+
                         for( int j = 0; j < clients.size(); j++ )
                         {
-                            sendMessage( *clients[j], addresses[i].toString() );
-                            sendMessage( *clients[j], receivedMessage );
+                            sf::Packet packet;
+                            packet << addresses[i].toString() << receivedMessage;
+                            clients[j]->send( packet );
+
+                            //std::cout << "Send " << addresses[i].toString() << " und " << receivedMessage << " to client " << j << std::endl;
                         }
                     }
                 }
